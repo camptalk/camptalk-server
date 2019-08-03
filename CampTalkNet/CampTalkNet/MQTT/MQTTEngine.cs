0e8e06ec2eaf339dcd4962395027f97336894f60
Rinkako <jiajidi@163.com>
2019-08-03 12:45:15 +0800

    Update: mqtt server fac

---
+++ CampTalkNet/CampTalkNet/MQTT/MQTTEngine.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CampTalkNet.Service.Validator;
+using MQTTnet;
+using MQTTnet.Server;
+
+namespace CampTalkNet.MQTT
+{
+    internal class MqttEngine
+    {
+        public static MqttEngine GetInstance() => MqttEngine.syncEngine ?? (MqttEngine.syncEngine = new MqttEngine());
+
+        public async void Init()
+        {
+            if (this.IsInit)
+            {
+                return;
+            }
+            var mqttServer = new MqttFactory().CreateMqttServer();
+            var optionsBuilder = new MqttServerOptionsBuilder()
+                .WithConnectionValidator(c => AuthValidator.Auth(c.ClientId, c.Username, c.Password))
+                .WithConnectionBacklog(100)
+                .WithDefaultEndpointPort(1803);
+            await mqttServer.StartAsync(optionsBuilder.Build());
+            this.IsInit = true;
+        }
+
+        private MqttEngine() { }
+
+        public bool IsInit { get; private set; } = false;
+
+        private static MqttEngine syncEngine = null;
+    }
+}
---
+++ CampTalkNet/CampTalkNet/Service/Validator/AuthValidator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CampTalkNet.Service.Validator
+{
+    internal class AuthValidator
+    {
+        public static bool Auth(string clientId, string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
